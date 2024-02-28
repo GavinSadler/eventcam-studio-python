3682cc980d0901c187aa3ba055783694b772aa90
Gavin Sadler <35090401+GavinSadler@users.noreply.github.com>
2024-02-28 10:06:18 -0500

    Added a framerate test for the frame camera

---
+++ tests/framerate.py
@@ -0,0 +1,24 @@
+
+from time import sleep
+
+import numpy as np
+from FrameCamera import FrameCamera
+
+frameCam = FrameCamera()
+frameCam.connect()
+
+numFrames = 0
+
+def onFrameFrame(frame: np.ndarray):
+    global numFrames
+    numFrames += 1
+
+frameCam.registerCallback("dearpygui", onFrameFrame)
+
+frameCam.startStreaming()
+
+delay = 10
+
+sleep(delay)
+
+print(f"{numFrames} in {delay} seconds, {numFrames / delay} fps")
